700a39a1b13c3ccca9387009854389c21af93f2b
Dominic Cullen <dominic@tryflux.com>
2021-02-09 21:54:07 +0000

    feat(spotify api): complete spotify auth flow with implicit grant

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import index from './callback/index';
+
+index();
--- src/spotify/client.ts
+++ src/spotify/client.ts
@@ -1,16 +1,8 @@
-import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
-import Crypto from 'crypto';
-import base64url from 'base64url';
-import { spotifyClientId } from '../config';
+import axios, { AxiosInstance } from 'axios';
 
 export default class SpotifyClient {
 
     private _axios: AxiosInstance;
-    private _redirectUri = 'http://localhost:8000/callback';
-    private _scopes = [
-        'playlist-modify-public',
-        'playlist-modify-private'
-    ].join(' ');
 
     constructor() {
         this._axios = axios.create({
@@ -20,26 +12,6 @@ export default class SpotifyClient {
         this._axios.interceptors.response.use((res) => res.data)
     }
 
-    async authorize() : Promise<any> {
-        const codeVerifier = Crypto.randomBytes(64).toString('base64').slice(0, 64);
-        const codeChallenge = base64url.fromBase64(Crypto.createHash('sha256').update(codeVerifier).digest('base64'));
-        const state = Math.random().toString(36).substring(7);
-
-        const config: AxiosRequestConfig = {
-            url: `/authorize`,
-            method: 'get',
-            params: {
-                client_id: spotifyClientId,
-                response_type: 'code',
-                redirect_uri: this._redirectUri,
-                code_challenge_method: 'S256',
-                code_challenge: codeChallenge,
-                state: state,
-                scope: this._scopes
-            }
-        }
-
-        return this._axios.request<any, any>(config)
-    }
+    
 
 }

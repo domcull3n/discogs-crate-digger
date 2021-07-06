7e6b0c5567cdcc319f0c4acfc076396089cb9827
domcull3n <dcullen97@gmail.com>
2021-07-06 10:49:06 +0100

    pass username from cli arg to pull discogs inventory

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,19 @@
 // import path from 'path';
 // import index from './callback/index';
 import { Command } from 'commander';
+import Service from './service';
 
 const program = new Command();
+const service = new Service();
+
 program
     .arguments('<username>')
     .description('discogs-crate-digger', {
         username: 'the marketplace user you want to dig through',
     })
     .action((username: string) => {
-        console.log(username);
+        const inventory = service.run(username);
+        void inventory.then((res) => console.log(res));
     });
 
 program.parse();
--- src/service.ts
+++ src/service.ts
@@ -1,8 +1,14 @@
-// import DiscogsClient from './discogs/client';
+import DiscogsClient from './discogs/client';
+import { Inventory } from './discogs/models/marketplace';
 
 export default class Service {
-    // private discogsClient: DiscogsClient;
-    // constructor() {
-    //     this.discogsClient = new DiscogsClient();
-    // }
+    private discogsClient: DiscogsClient;
+
+    constructor() {
+        this.discogsClient = new DiscogsClient();
+    }
+
+    async run(discogsUsername: string): Promise<Inventory> {
+        return await this.discogsClient.getInventory(discogsUsername);
+    }
 }

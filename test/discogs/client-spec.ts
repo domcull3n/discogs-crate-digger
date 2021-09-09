dc87344a00ec28eaa087b4751e69ac5e22457d66
domcull3n <dcullen97@gmail.com>
2021-09-09 15:12:31 +0100

    fix test

--- test/discogs/client-spec.ts
+++ test/discogs/client-spec.ts
@@ -1,11 +1,12 @@
 import { assert } from 'chai';
 import DiscogsClient from '../../src/discogs/client';
-import { Inventory } from '../../src/discogs/models/marketplace';
+import { Inventory } from '../../src/discogs/models/generic';
+import { InventoryType } from '../../src/models';
 
 describe('DiscogsClient', () => {
     it('should return an inventory', async () => {
         const discogsClient = new DiscogsClient();
-        const inventory: Inventory = await discogsClient.getInventory('crazybeatrecords', 1);
+        const inventory: Inventory = await discogsClient.getInventory('crazybeatrecords', InventoryType.MARKETPLACE, 1);
 
         assert(inventory != null);
     });

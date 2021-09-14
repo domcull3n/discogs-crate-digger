5770083bcec0a7582ae41f48101087d950624c9c
domcull3n <dcullen97@gmail.com>
2021-09-14 11:50:05 +0100

    Add in option in commander to specify genres

--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,35 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 
 import index from './callback/index';
-import { Command } from 'commander';
-import { InventoryType } from './models';
+import { Command, Option } from 'commander';
+import { DiscogsMainGenres, InventoryType } from './models';
 
 const program = new Command();
 
 program
     .argument('<username>', "the user who's marketplace/collection you want to dig through")
     .option('--collection', 'dig through the users collection if public (default is marketplace)')
+    .addOption(
+        new Option(
+            '--genres <genres...>',
+            "genre's to pull out of the specified collection (only works with collections)",
+        ).choices(DiscogsMainGenres),
+    )
+
     .action((username: string, options) => {
+        if (options.collection === undefined && options.genres !== undefined) {
+            console.error("Genre's can't be specified with an marketplace, only an inventory");
+            console.error('Exiting...');
+            return;
+        }
         try {
             index({
                 username: username.toLowerCase(),
                 inventoryType: options.collection ? InventoryType.COLLECTION : InventoryType.MARKETPLACE,
+                discogsGenres: options.genres,
             });
         } catch (error) {
-            console.log('an error has occured, please try again later.');
+            console.error('an error has occured, please try again later.');
         }
     });
 
--- src/models.ts
+++ src/models.ts
@@ -7,4 +7,23 @@ export enum InventoryType {
 export interface CommandOptions {
     username: string;
     inventoryType: InventoryType;
+    discogsGenres?: string[];
 }
+
+export const DiscogsMainGenres = [
+    'Blues',
+    'Brass & Military',
+    "Children's",
+    'Classical',
+    'Electronic',
+    'Folk, World, & Country',
+    'Funk / Soul',
+    'Hip Hop',
+    'Jazz',
+    'Latin',
+    'Non-Music',
+    'Pop',
+    'Reggae',
+    'Rock',
+    'Stage & Screen',
+];
